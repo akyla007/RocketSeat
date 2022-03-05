187ef5abe52bace324f90da5fdd6d022eb9621ec
Akyla <akylaaquino@hotmail.com>
2022-03-05 09:32:10 -0300

    modified:   package.json 	modified:   src/App.jsx 	new file:   src/components/Counter.jsx 	new file:   src/components/RepositoryItem.jsx 	modified:   src/components/RepositoryList.jsx 	modified:   webpack.config.js 	modified:   yarn.lock

--- ReactJs/01-github-explorer/webpack.config.js
+++ ReactJs/01-github-explorer/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
-const htmlWebPackPlugin = require('html-webpack-plugin')
+const htmlWebPackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebPackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+
 const isDevelopment = process.env.NODE_ENV !== 'production';
 module.exports = {
     mode: isDevelopment ? 'development' : 'production',
@@ -14,23 +17,32 @@ module.exports = {
     },
     devServer: {
         static: path.resolve(__dirname, 'public'),
+        hot: true,
     },
     plugins: [
+        isDevelopment && new ReactRefreshWebPackPlugin(),
         new htmlWebPackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
         })
-    ],
+    ].filter(Boolean),
     module: {
         rules: [
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
-                use: 'babel-loader',
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    }
+                },
             },
             {
                 test: /\.scss$/,
                 exclude: /node_modules/,
-                use: ['style-loader','css-loader', 'sass-loader'],
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             }
         ]
     }
