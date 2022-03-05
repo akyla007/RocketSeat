768539c2ba293b38a78b68533b5c7ed809bf6ac2
Akyla <akylaaquino@hotmail.com>
2022-03-05 08:33:37 -0300

    modified:   package.json 	modified:   src/App.jsx 	new file:   src/components/RepositoryList.jsx 	renamed:    styles/global.scss -> src/styles/global.scss 	modified:   yarn.lock

---
+++ ReactJs/01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,38 @@
+const RepositoryName = 'unform'
+
+export function RepositoryList(){
+    return (
+        <section class-name="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório    
+                    </a>
+                </li>
+
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório    
+                    </a>
+                </li>
+
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório    
+                    </a>
+                </li>
+            </ul>
+        </section>
+    );
+}
