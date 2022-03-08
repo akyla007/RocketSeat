037c48abe5c63c212a7dce21854b9e673a588073
Akyla <akylaaquino@hotmail.com>
2022-03-08 10:12:34 -0300

    modified:   dist/bundle.js 	modified:   src/App.jsx 	deleted:    src/components/Counter.jsx 	modified:   src/components/RepositoryItem.jsx 	modified:   src/components/RepositoryList.jsx 	new file:   src/styles/repositories.scss 	modified:   webpack.config.js 	deleted:    yarn-error.log

--- ReactJs/01-github-explorer/src/components/RepositoryList.jsx
+++ ReactJs/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,4 +1,6 @@
 import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
 
 const repository = {
     name: 'unform',
@@ -7,8 +9,16 @@ const repository = {
 }
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, [repositories])
+
     return (
-        <section class-name="repository-list">
+        <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
