035b8d070489f071f2e45de7a177489e3397114a
Akyla <akylaaquino@hotmail.com>
2022-03-15 11:11:34 -0300

    modified:   src/repositories/CategoriesRepository.ts 	modified:   src/routes/categories.routes.ts

--- NodeJs/Projeto-TS/src/repositories/CategoriesRepository.ts
+++ NodeJs/Projeto-TS/src/repositories/CategoriesRepository.ts
@@ -10,7 +10,7 @@ class CategoriesRepository {
     constructor() {
         this.categories = [];
     }
-    create({ name, description }: ICreateCategoryDTO) : void {
+    create({ name, description }: ICreateCategoryDTO): void {
         const category = new Category();
         Object.assign(category, {
             name,
@@ -19,6 +19,14 @@ class CategoriesRepository {
         })
 
         this.categories.push(category);
+    }
+    list(): Category[] {
+        return this.categories;
+    }
+
+    findByName(name: string): Category {
+        const category = this.categories.find((category) => category.name === name)
+        return category;
     }
 }
 
